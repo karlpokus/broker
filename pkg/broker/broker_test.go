532319f3b03b961d82865f1a918583eafa7609a0
Karl Pokus <karl.pokus@gmail.com>
2019-04-05 22:33:50 +0200

    extend the pubsub tests

--- pkg/broker/broker_test.go
+++ pkg/broker/broker_test.go
@@ -1,53 +1,19 @@
 package broker
 
 import (
-	"bytes"
-	"sync"
 	"testing"
+
+	"github.com/karlpokus/broker/pkg/bufw"
 )
-
-type mockWriter struct {
-	mu      sync.Mutex
-	buf     bytes.Buffer
-	written chan bool
-}
-
-// Write writes to an internal buffer for later inspection
-// also writes to the written chan if it has been instantiated
-func (mw *mockWriter) Write(b []byte) (n int, err error) {
-	mw.mu.Lock()
-	defer mw.mu.Unlock()
-	mw.buf.Write(b)
-	if mw.written != nil {
-		mw.written <- true
-	}
-	return len(b), nil
-}
-
-// Drain resets-, and returns the buffer contents
-func (mw *mockWriter) Drain() string {
-	mw.mu.Lock()
-	defer mw.mu.Unlock()
-	s := mw.buf.String()
-	mw.buf.Reset()
-	return s
-}
-
-// Wait blocks on the written chan, used for synchronization
-// must use if written chan has been instantiated or the Write call will block
-func (mw *mockWriter) Wait() bool {
-	return <-mw.written
-}
 
 func TestPubSub(t *testing.T) {
 	bkr := NewBroker(&brokerConf{})
-	w := &mockWriter{
-		written: make(chan bool),
-	}
+	w := bufw.New(true)
 	id, err := NewId()
 	if err != nil {
 		t.Errorf("%s", err)
 	}
+
 	// sub
 	m := []byte("sub;cats;")
 	err = bkr.Parse(m, w, id)
@@ -60,28 +26,48 @@ func TestPubSub(t *testing.T) {
 			t.Errorf("%s should be present in queue subs", id)
 		}
 	}
+
 	// subDupe
 	err = bkr.Parse(m, w, id)
 	if err != subDupe {
 		t.Errorf("got %s, want %s", err, subDupe)
 	}
-	// pub
+
+	// pub 1
 	m = []byte("pub;cats;bixa")
 	err = bkr.Parse(m, w, id)
 	if err != nil {
 		t.Errorf("%s", err)
 	}
-	bkr.ops <- func(s storage) {
-		msgs := len(s["cats"].msgs)
-		if msgs != 0 {
-			t.Errorf("got %d, want %d", msgs, 0)
-		}
-	}
+	bkr.ops <- msgsN(t)
 	w.Wait()
 	want := "bixa"
 	got := w.Drain()
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
+	}
+
+	// pub 2
+	m = []byte("pub;cats;rex")
+	err = bkr.Parse(m, w, id)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	bkr.ops <- msgsN(t)
+	w.Wait()
+	want = "rex"
+	got = w.Drain()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func msgsN(t *testing.T) opFunc {
+	return func(s storage) {
+		msgs := len(s["cats"].msgs)
+		if msgs != 0 {
+			t.Errorf("got %d, want %d", msgs, 0)
+		}
 	}
 }
 
